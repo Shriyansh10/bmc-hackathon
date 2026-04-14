7c54110d04ccff85b0d8197c1e71ce6f63eb406c
Shriyansh007 <agarwalshriyansh007@gmail.com>
2026-04-14 22:38:09 +0530

    server and db setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import http from 'node:http'
+import app from './src/app.js'
+import 'dotenv/config'
+import connectDB from './src/common/config/db.js'
+
+const port = process.env.PORT || 8000;
+const url = process.env.URL || '127.0.0.1'
+async function main(){
+
+    try{
+        const server = http.createServer(app)
+        const res =  await connectDB.query('SELECT NOW()');
+        console.log(res)
+        server.listen(port, url,() => console.log(`Server is running at Port ${port} in ${process.env.MODE} mode`))
+    }catch(error){
+        console.log(error);
+    }
+}
+
+main()
